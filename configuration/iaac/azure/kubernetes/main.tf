7319907f7f26f054857b000ab51433fb991d9b24
Ife-the-lion <olorunfemi_i@yahoo.com>
2022-06-23 09:29:24 +0100

    quota rebase

--- configuration/iaac/azure/kubernetes/main.tf
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -25,11 +25,7 @@ resource "azurerm_kubernetes_cluster" "terraform-k8s" {
   default_node_pool {
     name            = "agentpool"
     node_count      = var.node_count
-<<<<<<< HEAD
-    vm_size         = "Standard_GS5-8"
-=======
-    vm_size         = "standard_d16ads_v5"
->>>>>>> ffa4b2f (terraform fix)
+vm_size = "standard_a2_v2"
   }
 
   service_principal {
