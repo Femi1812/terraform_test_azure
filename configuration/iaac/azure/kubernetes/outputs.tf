4eba80a181e73f359887fe06b4bcabfacc7f64ff
Ife-the-lion <olorunfemi_i@yahoo.com>
2022-06-23 07:34:47 +0100

    output fix

--- configuration/iaac/azure/kubernetes/outputs.tf
+++ configuration/iaac/azure/kubernetes/outputs.tf
@@ -18,7 +18,7 @@ output "cluster_password" {
   value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.password
 }
 
-output "kube_config" {
+/* output "kube_config" {
   value = azurerm_kubernetes_cluster.terraform-k8s.kube_config_raw
 <<<<<<< HEAD
   sensitive = true
@@ -29,7 +29,7 @@ output "host" {
 }
 =======
 }
-
+ */
 output "host" {
   value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.host
 }
